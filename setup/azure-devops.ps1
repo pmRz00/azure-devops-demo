5e6c306ada4a35c67192d48ddbc93c460994a6cb
Torsten Stiller <tostille@microsoft.com>
2019-05-14 15:55:50 +0200

    changes repo to default azure devops repo

--- setup/azure-devops.ps1
+++ setup/azure-devops.ps1
@@ -2,4 +2,4 @@ az extension add --name azure-devops
 az extension show --name azure-devops
 az devops configure --defaults organization=https://dev.azure.com/tostille/
 az devops project create --name "multi-stage-builds"
-az repos import create --git-source-url "https://github.com/tpstiller/azure-devops-demo.git" --project "multi-stage-builds" --repository "azure-devops-demo"
+az repos import create --git-source-url "https://github.com/tpstiller/azure-devops-demo.git" --project "multi-stage-builds" --repository "multi-stage-builds"
