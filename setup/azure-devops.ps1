805b8b5dcfa62f7a192e995195e99407476e599b
Torsten Stiller <tostille@microsoft.com>
2019-05-14 11:15:25 +0200

    Added az cli calls to create a project and import this repo

--- setup/azure-devops.ps1
+++ setup/azure-devops.ps1
@@ -1,3 +1,5 @@
 az extension add --name azure-devops
 az extension show --name azure-devops
 az devops configure --defaults organization=https://dev.azure.com/tostille/
+az devops project create --name "multi-stage-builds"
+az repos import create --git-source-url "https://github.com/tpstiller/azure-devops-demo.git" --project "multi-stage-builds" --repository "azure-devops-demo"
